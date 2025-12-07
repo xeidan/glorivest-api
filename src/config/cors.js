46c031d9d81b2f4cfc717054c101686c166dfdf3
Daniel <daniel@Daniels-MacBook-Air.local>
2025-12-07 08:43:50 +0100

    a

--- src/config/cors.js
+++ src/config/cors.js
@@ -9,24 +9,23 @@ const allowedOrigins = [
   'https://www.glorivest.com',
   'http://localhost:3000',
   'http://127.0.0.1:5503',
+  'http://127.0.0.1:5503/',
 ];
 
 module.exports = cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin like mobile apps, curl, server-side
-    if (!origin) return callback(null, true);
+  if (!origin) return callback(null, true);
 
-    // exact matches
-    if (allowedOrigins.includes(origin)) return callback(null, true);
+  // allow localhost entirely
+  if (origin.includes('127.0.0.1') || origin.includes('localhost')) {
+    return callback(null, true);
+  }
 
-    // allow any localhost with any port
-    if (origin.startsWith('http://localhost') || origin.startsWith('http://127.0.0.1')) {
-      return callback(null, true);
-    }
+  if (allowedOrigins.includes(origin)) return callback(null, true);
 
-    console.error('❌ Blocked by CORS:', origin);
-    return callback(new Error('Not allowed by CORS'));
-  },
+  console.error('❌ Blocked by CORS:', origin);
+  return callback(new Error('Not allowed by CORS'));
+},
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
