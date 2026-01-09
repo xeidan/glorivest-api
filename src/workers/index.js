4ab0d4559881ec055bd157774a4ca43db82ec3de
Daniel <daniel@Daniels-MacBook-Air.local>
2026-01-09 19:58:11 +0100

    disable broken background workers

--- src/workers/index.js
+++ src/workers/index.js
@@ -1,10 +1,10 @@
-// src/workers/index.js
-'use strict';
+// // src/workers/index.js
+// 'use strict';
 
-require('dotenv').config();
+// require('dotenv').config();
 
-require('./poller.worker').start();
-require('./sweep.worker').start();
-require('./withdrawal.worker').start();
+// require('./poller.worker').start();
+// require('./sweep.worker').start();
+// require('./withdrawal.worker').start();
 
-console.log('🔥 All workers launched.');
+// console.log('🔥 All workers launched.');
