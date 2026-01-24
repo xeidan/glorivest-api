5af1474bde0833d41598c5fae0e60b8eeb48ede9
Daniel <daniel@Daniels-MacBook-Air.local>
2026-01-24 17:47:39 +0100

    a

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
+// AUTH
+router.use('/auth', require('./auth.routes'));
+
+// CORE
 router.use('/accounts', require('./account.routes'));
 router.use('/trades', require('./trade.routes'));
 
