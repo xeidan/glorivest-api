54eab13f8635a4ebd8ec99c9c70c5b1185daca72
Daniel <daniel@Daniels-MacBook-Air.local>
2026-02-08 22:17:44 +0100

    fix transfer route auth middleware import

--- src/routes/transfer.routes.js
+++ src/routes/transfer.routes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const { transferProfits } = require('../controllers/transfer.controller');
-const requireAuth = require('../middleware/requireAuth');
+const requireAuth = require('../middleware/auth');
 
 const router = express.Router();
 
