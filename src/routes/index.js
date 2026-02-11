7eec3f58ce945796060908e7c4b998d6cdfc4af8
Daniel <daniel@Daniels-MacBook-Air.local>
2026-02-11 14:08:37 +0100

    a

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ console.log('✅ routes/index.js loaded');
 const express = require('express');
 const router = express.Router();
 
-const authMiddleware = require('../middleware/auth.middleware');
+const authMiddleware = require('../middleware/auth')
 const { getPerformanceSummary } = require('../controllers/performance.controller');
 
 // AUTH
