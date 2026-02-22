e1f128af41e19af8bfb0abcd262c6484fddd921e
Daniel <daniel@Daniels-MacBook-Air.local>
2026-02-22 17:36:40 +0100

    Add deposit webhook route

--- src/routes/index.js
+++ src/routes/index.js
@@ -19,6 +19,7 @@ router.use('/cycle', require('./cycle.routes'));
 router.use('/transfer', require('./transfer.routes'));
 router.use('/performance', require('./performance.routes'));
 router.use('/leaderboard', require('./leaderboard.routes'));
+router.use('/deposit', require('./deposit.routes')); 
 router.use('/admin', require('./admin.routes'));
 
 module.exports = router;
