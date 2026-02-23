f51544bf2502cf0dbd94b18677f40f525e9590b3
Daniel <daniel@Daniels-MacBook-Air.local>
2026-02-23 13:54:30 +0100

    a

--- src/routes/deposit.routes.js
+++ src/routes/deposit.routes.js
@@ -4,15 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 const requireAuth = require('../middleware/auth');
+
 const {
   createDeposit,
-  markDepositPaid
+  markPaid
 } = require('../controllers/deposit.controller');
 
-// Create deposit
 router.post('/', requireAuth, createDeposit);
-
-// Mark deposit as paid
-router.post('/:depositId/mark-paid', requireAuth, markDepositPaid);
+router.post('/:depositId/mark-paid', requireAuth, markPaid);
 
 module.exports = router;
