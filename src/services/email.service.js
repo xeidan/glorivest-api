1b9c69e537fab449836b5b93934d4db3cb96eaee
Daniel <daniel@Daniels-MacBook-Air.local>
2026-03-17 23:04:55 +0100

    j

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -4,7 +4,7 @@ const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const FROM = 'onboarding@resend.dev';
+const FROM = 'Glorivest <no-reply@glorivest.com>';
 
 /**
  * CORE EMAIL SENDER
