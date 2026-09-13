f2be3101560242b56316f1094e0b6631ad557550
xeidan <dancicada5@gmail.com>
2026-09-13 14:42:04 +0100

    Fix CORS and remove mock market inserts

--- src/routes/marketSnapshot.routes.js
+++ src/routes/marketSnapshot.routes.js
@@ -4,11 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  recordMarketSnapshot,
   getMarketCandles
 } = require('../controllers/marketSnapshot.controller');
 
-router.post('/', recordMarketSnapshot);
 router.get('/candles', getMarketCandles);
 
 module.exports = router;
